8b59fe0d9ca9319773a3ff87da7f2ca6e4925758
elsanaa1 <sanaa.jerbouh1@gmail.com>
2025-08-12 17:31:36 +0100

    api random quote

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 
 import './App.css';
 import QuoteC from './QuoteC';
-
+import { useEffect,useState } from "react";
 
 function App() {
+const [quote,setQuote]=useState('');
+ 
+
+const getQuote=() => {
+ fetch("https://api.quotable.io/random")
+ .then(res => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then(data => {
+        setQuote(`${data.content} — ${data.author}`);
+      })
+      .catch(error => {
+        console.error("Error fetching quote:", error);
+      });
+  };
+
+
+
+useEffect( () => { 
+//  get random quote from quotes api :
+getQuote();
+},[])
+
   return (
     <div className="App">
       <QuoteC></QuoteC>
