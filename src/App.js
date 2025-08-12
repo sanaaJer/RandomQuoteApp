244a61e44ba55c6e77018bf6d39b79ce0ba51059
elsanaa1 <sanaa.jerbouh1@gmail.com>
2025-08-12 17:12:19 +0100

    quote componenet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import QuoteC from './QuoteC';
+
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteC></QuoteC>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/QuoteC.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function QuoteC() {
+  return (
+      <div class="cntr d-flex flex-column justify-content-center align-items-center ">
+        {/* <!-- quote part --> */}
+        <div class="quot-wid ">
+            <h1 class="text-center">Hello, I.m Sanaa,Hello, I.m Sanaa,Hello, I.m Sanaa</h1>
+            <p>-Autor</p>
+            <div class="row ">
+              <div class="col-1">j</div>
+              <div class="col-1">f</div>
+              <div class="col-2">f</div>
+            </div>
+        </div>
+        {/* <!-- developer name --> */}
+        <p>by Sanaa</p>
+    </div>
+
+  )
+}
